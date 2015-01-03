264037a2a35b45e9c9bde0c41453dda21a3130f2
lulu2184 <lulu2184@gmail.com>
2015-01-04 00:11:31 +0800

    add sql

---
+++ dishes.sql
@@ -0,0 +1,10 @@
+create table dishes(
+    name varchar(50),
+    category varchar(50),
+    mainIngredient varchar(20),
+    taste varchar(10),
+    effect varchar(10),
+    ingredients varchar(500)
+);
+
+
